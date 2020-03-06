f983d9919b7dab340a018194ddefe14b01f79d80
rishabaggarwal247 <rishabaggarwal247@gmail.com>
2020-03-07 04:32:15 +0530

    commit json

--- application.py
+++ application.py
@@ -10,9 +10,11 @@ def hello():
 
 @app.route("/Askme", methods=['POST'])
 def Askme():
-    data = request.get_data().decode("utf-8")
-    data = json.loads(data)
-    print(data)
+    try:
+        jsondata = request.get_data().decode("utf-8")
+        jsondata = json.loads(jsondata)
+    except Exeption  as e:
+        print(str(e))
 
     return jsonify({'msg': 'Fuck Off! Again', }), 401
 

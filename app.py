861062074eefb9fead4ed66a957d69e6332e6d3f
rishabaggarwal247 <rishabaggarwal247@gmail.com>
2020-03-07 21:05:13 +0530

    suggestion1.0

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, make_response, logging
 import json
 from main2 import *
-
+import random as rd
 
 
 
@@ -32,4 +32,12 @@ def Askme():
         print(str(e))
 
         return jsonify({'msg': 'try Again', }), 401
+
+@app.route("/suggestiontest", methods=['GET'])
+def suggestiontest():
+    list1=list([["How was your day today?","Superb","Satisfied","Regular (As usual)","Worst"],["Turnout of the quarrel?", "I won the debate","compromised for peace","forgot the fight and started talking","fight led to complications in relationship"],["Anticipation of next morning?","Excited (big day)","As usual (regular stuffs)","Nothing much","Exhausted"],["Do I like and enjoy my current physical state?","Absolutely","Look to improve everyday","Do look for motivation","low self-esteem"]])
+    i = rd.randint(0,len(list1)-1)
+    print(list1[i])
+    return jsonify({'msg': list1[i]}), 200
+
 
